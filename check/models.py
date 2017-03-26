4d3255868123377b29eb4653a396007c36694b97
taobaiyang <taobaiyang@gmail.com>
2017-03-26 14:11:25 +0800

    add login form

--- check/models.py
+++ check/models.py
@@ -2,7 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class user(models.Model):
-    name = models.CharField(max_length=20, null=True)
-    password = models.CharField(max_length=20, null=True)  # what about min length?
-    login_status = models.BooleanField(default=False) #default not login
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.username
--- check/views.py
+++ check/views.py
@@ -1,18 +1,40 @@
+# coding=utf-8
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from . import models
+from django import forms
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(label='用户名', max_length=100)
+    password = forms.CharField(label='密码', widget=forms.PasswordInput())
 
 
 def login_page(request):
-    return render(request, 'check/login.html')
+    if request.method == 'POST':
+        uf = UserForm(request.POST)
+        if uf.is_valid():
+            username = uf.cleaned_data['username']
+            password = uf.cleaned_data['password']
+            user = User.objects.filter(username__exact=username, password__exact=password)
+            if user:
+                return HttpResponse("1")
+            else:
+                return HttpResponse("2")
+    else:
+        uf = UserForm()
+    return render(request, 'check/login.html', {'form': uf})
 
 
-def login_action(request):
-    name = request.POST.get('name', 'USER')
-    password = request.POST.get('password', 'PWD')
+def login_action(requestuest):
+    name = requestuest.POST.get('name', 'USER')
+    password = requestuest.POST.get('password', 'PWD')
     models.user.objects.create(name='name', password='password')  # create a class
     user_info = models.user.objects.all()  # get all users' information
-    return render(request, 'check/login.html', {'user_info': user_info})  # jump to a new page, remain to design
+    return render(requestuest, 'check/login.html', {'user_info': user_info})  # jump to a new page, remain to design
 
 
-def mainpage(request):
-    return render(request, 'check/mainpage.html')
+def mainpage(requestuest):
+    return render(requestuest, 'check/mainpage.html')
